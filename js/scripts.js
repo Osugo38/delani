4d6bda1d3a39d9b04dfd12f8c6a58fc975e86031
Joseck Osugo <josugo38@gmail.com>
2019-07-21 11:51:35 +0300

    add a script for toggling first icon

--- js/scripts.js
+++ js/scripts.js
@@ -10,38 +10,3 @@ $(document).ready(function(){
 
 });
 });
-
-$(document).ready(function(){
-  $(".clic2").click(function(){
-    $("#toto2").toggle();
-      $(".clic2").toggle();
-      });
-
-      $("#toto2").click(function(){
-        $(".clic2").show();
-          $("#toto2").hide();
-
-});
-});
-
-$(document).ready(function(){
-  $(".clic3").click(function(){
-    $("#toto3").toggle();
-      $(".clic3").toggle();
-      });
-
-      $("#toto3").click(function(){
-        $(".clic3").show();
-          $("#toto3").hide();
-
-});
-});
-
-// below is the user Interface logic
-
-$(document).ready(function(){
-  $("form").submit(function(event){
-    event.preventDefault();
-    alert("We have received your message.Thank you for reaching out to us!." );
-  });
-});
