00cb065835c20ebf93fd61530409e24af2e0c497
Joseck Osugo <josugo38@gmail.com>
2019-07-21 23:16:02 +0300

    add ascreenshot of the project and also modify portfolio icons

--- js/scripts.js
+++ js/scripts.js
@@ -45,3 +45,50 @@ $(document).ready(function(){
     alert("We have received your message.Thank you for reaching out to us!." );
   });
 });
+
+
+/*
+$('#pro4').on("mouseover" , function(){
+        $('.probox').show();
+     });
+
+    $('#pro4').on("mouseout", function(){;
+       $('#probox').hide();
+     });
+
+
+
+$(document).ready(function(){
+  $(".wrap1").mouseover(function(){
+    $("#overlay1").show();
+  }).mouseout(function(){
+    $("#overlay1").hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".wrap2").mouseover(function(){
+    $("#overlay2").show();
+  }).mouseout(function(){
+    $("#overlay2").hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".wrap3").mouseover(function(){
+    $("#overlay3").show();
+  }).mouseout(function(){
+    $("#overlay3").hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".wrap4").mouseover(function(){
+    $("#overlay4").show();
+  }).mouseout(function(){
+    $("#overlay4").hide();
+  });
+});
+
+
+*/
